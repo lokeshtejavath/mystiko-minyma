5b86019b9bbddbb296aee492eed8756131bcbcaf
lokeshtejavath <lokinaikkumar.t@gmail.com>
2021-12-16 12:48:19 +0530

    completed decryption module

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import insert
 import randstring
-
+import retrive
 i = insert.Inserter()
 key = randstring.randstring()
-i.insert("hello", key, "./loki.jpeg", "out")
-print(key)
+# i.insert("let's do this", key, "./loki.jpeg", "out")
+# print(key)
+key = "098oinKJkT2CjFd8bvdN6kFLwYDM"
+r = retrive.Retrive()
+r.retriver(key, "./out.png")
--- retrive.py
+++ retrive.py
@@ -5,7 +5,6 @@ class Retrive:
 
     def retriver(self, key: str, image: str):
         initaliser = int(key[0:3])
-
         img = cv2.imread(image)
         shapex = img.shape[0]
         shapey = img.shape[1]
@@ -13,7 +12,7 @@ class Retrive:
         convert = converter.converter()
         currentKey = []
         current = ""
-        counter = 8
+        counter = 0
         while counter != 8:
             if img[x][y][0] & 1:
                 current += "1"
@@ -28,6 +27,7 @@ class Retrive:
                     counter = 0
                     currentKey.append(initaliser ^ num)
                     initaliser = num
+                    current = ""
             x = ((x + 1) % shapex)
             if x == 0:
                 y += 1
@@ -37,4 +37,28 @@ class Retrive:
         if key != ans:
             print("wrong key")
             return
-        
+        currentKey = []
+        counter = 0
+        current = ""
+        while counter != 8:
+            if img[x][y][0] & 1:
+                current += "1"
+            else:
+                current += "0"
+            if len(current) == 8:
+                num = convert.bintodec(current)
+                if num == 0:
+                    counter += 1
+                    current = ""
+                else:
+                    counter = 0
+                    currentKey.append(initaliser ^ num)
+                    initaliser = num
+                    current = ""
+            x = ((x + 1) % shapex)
+            if x == 0:
+                y += 1
+        ans = ""
+        for i in currentKey:
+            ans += chr(i)
+        print("message:" + ans)
